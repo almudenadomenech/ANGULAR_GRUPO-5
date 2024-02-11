978c2639b34e63f3670cd04116e4ae1c1814e05e
almudenadomenech <violeta_33@hotmail.es>
2024-02-11 15:03:11 +0100

    rutas house

--- FRONTED/src/app/app.routes.ts
+++ FRONTED/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HouseListComponent } from './house-list/house-list.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'house',
+        component: HouseListComponent
+    },
+    {
+        path: 'house/:id',
+        component: HouseDetailComponent
+    }
+];

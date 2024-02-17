dd7c6c4eabc568f685fd0535569b98ba096364fe
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-02-17 13:21:05 +0100

    Propiedades y métodos para el componente

--- FRONTED/src/app/booking-list/booking-list.component.ts
+++ FRONTED/src/app/booking-list/booking-list.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './booking-list.component.css'
 })
 export class BookingListComponent {
-
+// Propiedades y métodos para el componente
+}
+export interface Booking {
+  id: number;
+  customerName: string;
+  date: Date;
+  startTime: string; // Hora de inicio de la reserva
+  endTime: string; // Hora de fin de la reserva
+  status: 'pending' | 'confirmed' | 'cancelled'; // Estado de la reserva
+  roomNumber: number; // Número de la habitación reservada
+  totalPrice: number; // Precio total de la reserva
+  phoneNumber: string; // Número de teléfono del cliente
+  email: string; // Correo electrónico del cliente
+  notes: string; // Notas adicionales sobre la reserva
+  // Otros campos relevantes
 }

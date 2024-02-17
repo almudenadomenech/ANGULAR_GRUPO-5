cdc0a9382d088f4ed0905641a448dde2f2ab9bf0
almudenadomenech <violeta_33@hotmail.es>
2024-02-17 10:23:34 +0100

    navbar y enrutamiento de casas y reservas

--- FRONTED/src/app/app.routes.ts
+++ FRONTED/src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { HouseListComponent } from './house-list/house-list.component';
 import { HouseDetailComponent } from './house-detail/house-detail.component';
+import { BookingListComponent } from './booking-list/booking-list.component';
+import { BookingDetailComponent } from './booking-detail/booking-detail.component';
 
 export const routes: Routes = [
     {
-        path: 'house',
+        path: 'houses',
         component: HouseListComponent
     },
     {
-        path: 'house/:id',
+        path: 'houses/:id',
         component: HouseDetailComponent
+    },
+    {
+        path: 'bookings',
+        component: BookingListComponent
+    },
+    {
+        path: 'bookings/:id',
+        component: BookingDetailComponent
     }
 ];

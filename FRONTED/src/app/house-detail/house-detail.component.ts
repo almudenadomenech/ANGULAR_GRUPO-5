f849e963f8cdfd08f073561666162910486c902d
suslobo <148755303+suslobo@users.noreply.github.com>
2024-02-20 12:02:50 +0100

    problemas on el enrutamiento en ts solucionado

--- FRONTED/src/app/house-detail/house-detail.component.ts
+++ FRONTED/src/app/house-detail/house-detail.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { House } from '../interfaces/house.model';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-house-detail',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './house-detail.component.html',
   styleUrl: './house-detail.component.css'
 })
-export class HouseDetailComponent implements OnInit{
+export class HouseDetailComponent implements OnInit {
   // atributos
+  /*
     houses: House[] = [];   
+house: any;
     
   // constructor con httpClient
     constructor(private http: HttpClient) {};
@@ -21,6 +24,19 @@ export class HouseDetailComponent implements OnInit{
       this.http.get<House[]>('http://localhost:3000/houses').subscribe(houses => this.houses = houses);
 
     }
+*/
+  house: House | undefined;
+
+  constructor(private http: HttpClient,
+    private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id'];
+      this.http.get<any>(`http://localhost:3000/houses/${id}`).subscribe(house => this.house = house);
+    }
+    );
+  }
 
 }
 

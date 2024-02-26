25da14fd8fa60b10ae0eb23a79477b23302c8f25
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-02-26 19:04:25 +0100

    cambios

--- FRONTED/src/app/app.component.ts
+++ FRONTED/src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HouseDetailComponent } from "./house-detail/house-detail.componen
-import { NavbarComponent } from "./navbar/navbar.component";t";
+import { HouseDetailComponent } from "./house-detail/house-detail.component"
+import { NavbarComponent } from "./navbar/navbar.component";
+import { BookingListComponent } from './booking-list/booking-list.component';
 
 {
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, BookingListModule, NavbarComponent]
+    selector: 'app-root'
+    standalone: true
+    templateUrl: './app.component.html'
+    styleUrl: './app.component.css'
+    imports: [RouterOutlet, NavbarComponent, BookingListComponent]
 }   imports: [RouterOutlet, HouseDetailComponent]
-})
+
 export class AppComponent {
   title = 'FRONTED';
 }

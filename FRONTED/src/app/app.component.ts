bd175b7f613e8675cebfc5e94f312507ba9542c0
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-01 19:04:40 +0100

    cambios

--- FRONTED/src/app/app.component.ts
+++ FRONTED/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HouseDetailComponent } from "./house-detail/house-detail.component"
 import { NavbarComponent } from "./navbar/navbar.component";
 import { BookingListComponent } from './booking-list/booking-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 {
     selector: 'app-root'
     standalone: true
     templateUrl: './app.component.html'
     styleUrl: './app.component.css'
-    imports: [RouterOutlet, NavbarComponent, BookingListComponent]
+    imports: [RouterOutlet, NavbarComponent,FooterComponent, BookingListComponent]
 }   imports: [RouterOutlet, HouseDetailComponent]
 
 export class AppComponent {

e04b669c5e0006b53aefe42c2f429eccaa78af5f
almudenadomenech <violeta_33@hotmail.es>
2024-03-02 13:15:09 +0100

    house form

--- FRONTED/src/app/house-form/house-form.component.ts
+++ FRONTED/src/app/house-form/house-form.component.ts
@@ -83,17 +83,23 @@ export class HouseFormComponent implements OnInit{
     }
 
     console.log(houses);
-    /* if(this.isUpdate){
+
+     /* if(this.isUpdate){
       const urlForUpdate = 'http://localhost:3000/houses/' + houses.id;
       this.httpClient.put<House>(urlForUpdate, houses).subscribe(data => this.router.navigate(['/']));
     } else {
-      const url = 'http://localhost:3000/phouses';
+      const url = 'http://localhost:3000/houses';
         this.httpClient.post<House>(url, houses).subscribe(data => this.router.navigate(['/']));
-    } */
+    }  */
     const url = 'http://localhost:3000/houses'
-    this.httpClient.post<House>(url, houses).subscribe(data => console.log(data));
+    this.httpClient.post<House>(url, houses).subscribe(data => console.log(data)); 
     
-
   }
-
+  compareObjects(o1: any, o2: any): boolean {
+    if (o1 && o2) {
+      return o1.id === o2.id;
+    } else {
+      return o1 === o2;
+    }
+  }
 }

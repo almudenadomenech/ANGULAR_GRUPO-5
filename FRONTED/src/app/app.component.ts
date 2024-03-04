0a51f20b5a9921c0770044bbf6171abced7792b1
Ekagarcian <156067063+Ekagarcian@users.noreply.github.com>
2024-03-04 12:13:41 +0100

    camvbios en Navbar y Footer

--- FRONTED/src/app/app.component.ts
+++ FRONTED/src/app/app.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HouseDetailComponent } from "./house-detail/house-detail.component"
-import { NavbarComponent } from "./navbar/navbar.component";
-import { BookingListComponent } from './booking-list/booking-list.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HouseDetailComponent } from "./house-detail/house-detail.component";
+import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 
-{
-    selector: 'app-root'
-    standalone: true
-    templateUrl: './app.component.html'
-    styleUrl: './app.component.css'
-    imports: [RouterOutlet, NavbarComponent,FooterComponent, BookingListComponent]
-}   imports: [RouterOutlet, HouseDetailComponent]
 
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HouseDetailComponent,RouterLink,NavbarComponent,FooterComponent ]
+})
 export class AppComponent {
   title = 'FRONTED';
 }
--- FRONTED/src/app/navbar/navbar.component.ts
+++ FRONTED/src/app/navbar/navbar.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
-  selector: 'app-navbar',
-  standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgbDropdownModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+    selector: 'app-navbar',
+    standalone: true,
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css',
+    imports: [FooterComponent]
 })
 export class NavbarComponent {
 
-  collapsed = true;
 }

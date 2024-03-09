26374257dc491d4fed893ad7b2bbd8e511f25c6d
almudenadomenech <violeta_33@hotmail.es>
2024-03-09 11:28:47 +0100

    booking-form app component.ts

--- FRONTED/src/app/app.component.ts
+++ FRONTED/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HouseDetailComponent } from "./house-detail/house-detail.component";
+import { BookingFormComponent } from './booking-form/booking-form.component';
 
 
 @Component({
@@ -8,7 +9,7 @@ import { HouseDetailComponent } from "./house-detail/house-detail.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HouseDetailComponent]
+    imports: [RouterOutlet, HouseDetailComponent, BookingFormComponent]
 })
 export class AppComponent {
   title = 'FRONTED';

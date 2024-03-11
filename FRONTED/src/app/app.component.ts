74a59471730960a4a3d8cec1c07899de8e01289c
almudenadomenech <violeta_33@hotmail.es>
2024-03-11 10:30:08 +0100

    Botones añadidos

--- FRONTED/src/app/app.component.ts
+++ FRONTED/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HouseDetailComponent } from './house-detail/house-detail.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HouseFormComponent } from './house-form/house-form.component';
 import { FooterComponent } from './footer/footer.component';
+import { BookingFormComponent } from './booking-form/booking-form.component';
 
 
 
@@ -12,7 +13,9 @@ import { FooterComponent } from './footer/footer.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, RouterLink, HouseDetailComponent, NavbarComponent, HouseFormComponent, FooterComponent]
+    imports: [RouterOutlet, RouterLink, HouseDetailComponent, 
+      NavbarComponent, HouseFormComponent, FooterComponent, 
+      BookingFormComponent]
 })
 export class AppComponent {
   title = 'FRONTED';

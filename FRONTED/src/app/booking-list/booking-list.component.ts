3b15ee989fd81f292f9ba1bf905e17daf1252de8
almudenadomenech <violeta_33@hotmail.es>
2024-03-13 13:23:53 +0100

    booking-list

--- FRONTED/src/app/booking-list/booking-list.component.ts
+++ FRONTED/src/app/booking-list/booking-list.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Booking } from '../interfaces/booking.model';
 
 @Component({
   selector: 'app-booking-list',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HttpClientModule],
   templateUrl: './booking-list.component.html',
   styleUrl: './booking-list.component.css'
 })
-export class BookingListComponent {
+export class BookingListComponent implements OnInit{
+
+  bookings: Booking [] = [];
+
+  constructor(private httCliente: HttpClient){}
+  ngOnInit(): void {
+    this.httCliente.get<Booking[]>('http://localhost:3000/booking')
+    .subscribe(bookings => this.bookings = bookings);
+  }
 
 }
